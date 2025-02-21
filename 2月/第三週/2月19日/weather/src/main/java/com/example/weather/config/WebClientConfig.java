9e27b2552aeee5e70b4cf37e25d3336cf8c559f7
INFlive <INFlive.tw@gmail.com>
2025-02-21 16:21:53 +0800

    2月21更新

--- 2月/第三週/2月19日/weather/src/main/java/com/example/weather/config/WebClientConfig.java
+++ 2月/第三週/2月19日/weather/src/main/java/com/example/weather/config/WebClientConfig.java
@@ -1,28 +1,22 @@
 package com.example.weather.config;
 
-import org.springframework.beans.factory.annotation.Value; //新增Value
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration // @Configuration 註解
+@Configuration
 public class WebClientConfig {
     
-    @Value("${weather.api.base-url}") //在application.yml使用weather底下的api底下的base-url網址
-    private String baseUrl;
+    private final WeatherProperties weatherProperties;
+
+    public WebClientConfig(WeatherProperties weatherProperties) {
+        this.weatherProperties = weatherProperties;
+    }
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-            .baseUrl(baseUrl)
-            .build();
-}
-    /*
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .baseUrl("https://api.openweathermap.org/data/2.5")
+                .baseUrl(weatherProperties.getBaseUrl())
                 .build();
     }
-    */
 }
